8d081b94fd143f355826daa0e914039440a0ae61
CodenificenT <xristianty@gmail.com>
2020-06-29 06:26:18 -0400

    last update

--- hashes.rb
+++ hashes.rb
@@ -1,3 +1,20 @@
 #  hashes are unordered, object-indexed collection of objects
 # or key-value pairs
 
+car = {
+    'brand' => 'Ford',
+    'model' => 'Mustang',
+    'color ' => 'blue',
+    'interior' => 'tan'
+}
+
+puts car['interior']
+puts car['model']
+
+car['doors'] = 2
+
+puts car
+
+puts car.keys
+
+puts car.values
