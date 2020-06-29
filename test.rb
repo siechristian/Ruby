8a51a41a41d7f80f38a9877349c4ee841bc5dc34
CodenificenT <xristianty@gmail.com>
2020-06-29 04:13:18 -0400

    initial setup

--- test.rb
+++ test.rb
@@ -1,1 +1,26 @@
 puts "I am running now!"
+
+# Strings in Ruby
+# append Strings
+
+greeting = "Hello"
+greeting << ' '
+greeting << 'World!'
+
+puts greeting
+
+puts "Yada " * 3
+
+puts "Blah " * 3
+
+puts 1 * 4
+
+puts " 1 " * 4
+
+# Strings escaping and interpolation
+
+puts "Let's escape!"
+
+puts 'Let\'s escape!'
+
+puts "I said, \"Lets escape\""
