a7f55e34f6b00293db6b7a59412961718139cc07
CodenificenT <xristianty@gmail.com>
2020-06-29 04:30:10 -0400

    arrays

--- arrays.rb
+++ arrays.rb
@@ -8,3 +8,18 @@ my_array = ['a', 'b', 'c', 100]
 
 puts my_array[2]
 
+array = [2, 4, ['a', 'c'], nil, 4, 'c']
+
+puts array.length
+puts array.size
+array.reverse
+puts array
+
+puts array.shuffle! #permanent
+
+# array.uniq, the ! operator makes changes permanent, changed in place
+# other array methods are array.compact (removes duplicates), array.flatten(break it apart)
+
+puts array.include?(1)
+
+puts array.delete_at(1)
